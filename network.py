2a876e78b4e15cf5fb9892fa4066b0eeb0305d5f
Jehan <jehan.piaget@epfl.ch>
2024-05-10 17:57:46 +0200

    Fin de la partie une

--- network.py
+++ network.py
@@ -1,1 +1,23 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+
 class FeedForwardNN(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super(FeedForwardNN, self).__init__()
+
+        self.layer1 = nn.Linear(in_dim, 64)
+        self.layer2 = nn.Linear(64, 64)
+        self.layer3 = nn.Linear(64, out_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, obs):
+        if isinstance(obs, np.ndarray):
+            obs = torch.tensor(obs, dtype=torch.float)
+
+        activation1 = self.relu(self.layer1(obs))
+        activation2 = self.relu(self.layer2(activation1))
+        output = self.layer3(activation2)
+
+        return output
